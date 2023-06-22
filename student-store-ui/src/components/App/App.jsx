c47a55ea10eee6cb1b27280dc6957276eca853b9
Daisy Trejo-Hernandez <dtrejohernandez@meta.com>
2023-06-22 00:26:58 -0700

    display banner(hero), created hero.jsx and hero.css files

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -4,18 +4,19 @@ import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
 import "./App.css"
+import Hero from "../Hero/Hero"
 
 export default function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <main>
-          {
-            
-          }
+          {/* YOUR CODE HERE! */}
           <Navbar/>
-          <Sidebar />
+          <Hero/>
+          <Sidebar/>
           <Home />
+
         </main>
       </BrowserRouter>
     </div>
