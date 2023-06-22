0d903c2ea24d5a635ce08a62fc5c3cf8b36be814
Daisy Trejo-Hernandez <dtrejohernandez@meta.com>
2023-06-22 16:54:58 -0700

    Display search and its button, display categories buttons and fixed contact us section image to display on the wide

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -4,6 +4,45 @@ import "./Home.css"
 export default function Home() {
   return (
     <div className="home">
+
+      <div className="sub-navbar">
+        <div className="content">
+          <div className="row">
+            <div className="searchbar">
+              <input type="text" placeholder="Search"/>
+              <button>Search</button>
+            </div>
+            <div className="links">
+              <span className="help"></span>
+              <div className="cart"></div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="hamburger-icon"></div>
+            <ul className="categories-menu">
+              <li className="is-active">
+                <button>All Categories</button>
+              </li>
+              <li>
+                <button>Clothing</button>
+              </li>
+              <li>
+                <button>Food</button>
+              </li>
+              <li>
+                <button>Accessories</button>
+              </li>
+              <li>
+                <button>Tech</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+
+
       <div className="about">
         <div className="content">
           <h3>About</h3>
@@ -29,6 +68,8 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      
 
       <div className="contact">
         <div className="content">
