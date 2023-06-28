c6188da2494002bb88d46b1da558c6aae5ac4b2b
Daisy Trejo-Hernandez <dtrejohernandez@meta.com>
2023-06-28 11:05:43 -0700

    Added icons to the side bar and fixed some styling issues with the sidebar and navbar

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -4,6 +4,21 @@ import "./Sidebar.css"
 export default function Sidebar() {
   return (
     <section className="sidebar">
+      <button i className="material-icons">arrow_forward</button>
+      
+      <div className="side-icons">
+        <div className="shopping-cart">
+          <button i className="material-icons">add_shopping_cart</button>
+        </div>
+        <div className="payment">
+          <button i className="material-icons">monetization_on</button>
+        </div>
+        <div className="checkout">
+          <button i className="material-icons">fact_check</button>
+        </div>
+       
+      </div>
+
     </section>
   )
 }
